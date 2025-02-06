510407bbc107487662773e35817070dd356ea76b
John Banyweire <johnelipse472@gmail.com>
2025-02-05 23:48:30 -0800

    first commit

---
+++ app/(backend)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import DashBoard from "@/components/back/dashboard";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <DashBoard />
+    </div>
+  );
+}
---
+++ app/(backend)/layout.tsx
@@ -0,0 +1,20 @@
+import SideBar from "@/components/back/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import React, { ReactNode } from "react";
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <SidebarProvider>
+      <div
+        className='flex w-full min-h-screen overflow-hidden">
+        <div className="flex-shrink-0'
+      >
+        <div className="flex-shrink-0">
+          <SideBar />
+        </div>
+
+        <main className=" md:flex-grow">{children}</main>
+      </div>
+    </SidebarProvider>
+  );
+}
